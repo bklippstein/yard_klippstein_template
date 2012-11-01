35e1fd5e3a005f1ac53508d0ce0a497ec8281fe3
bklippstein <klippstein@klippstein.com>
2012-11-01 09:40:41 +0100

    ---

--- templates/default/module/setup.rb
+++ templates/default/module/setup.rb
@@ -1,6 +1,6 @@
 
 def init
-  sections :header, :box_info, :pre_docstring, T('docstring'), :children,
+  sections :header, :box_info, :children, :pre_docstring, T('docstring'), 
     :attribute_summary, [:item_summary], :inherited_attributes,
     :method_summary, [:item_summary], :inherited_methods,
     :constant_summary, [T('docstring')], :inherited_constants,    
--- version.rb
+++ version.rb
@@ -1,7 +1,7 @@
 
 module YardKlippsteinTemplate  
   
-    VERSION = '0.0.26' 
+    VERSION = '0.0.27' 
     
 end
 
