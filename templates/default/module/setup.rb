d196935e1e2d20cf6ad8d9d27af71f465c89c82a
bklippstein <klippstein@klippstein.com>
2012-11-02 13:00:18 +0100

    ---

--- templates/default/module/setup.rb
+++ templates/default/module/setup.rb
@@ -1,11 +1,10 @@
 
 def init
-  sections :project_header, :header, :box_info, :children, :pre_docstring, T('docstring'), 
+  sections :header, :box_info, :children, :pre_docstring, T('docstring'), 
     :attribute_summary, [:item_summary], :inherited_attributes,
     :method_summary, [:item_summary], :inherited_methods,
     :constant_summary, [T('docstring')], :inherited_constants,    
     :methodmissing, [T('method_details')],
     :attribute_details, [T('method_details')],
-    :method_details_list, [T('method_details')],
-    :project_footer
+    :method_details_list, [T('method_details')]
 end
